a3e5e7171193cad56c56d46f245271644fb35283
rjsaa <rjsaavedran@gmail.com>
2023-05-06 00:03:27 -0700

    version

--- security/auth-service/src/main/java/com/cliente/authservice/security/SecurityConfig.java
+++ security/auth-service/src/main/java/com/cliente/authservice/security/SecurityConfig.java
@@ -5,8 +5,10 @@
 package com.cliente.authservice.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  *
@@ -14,8 +16,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
+   
+    @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
